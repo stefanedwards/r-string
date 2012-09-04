ad4ec5c65c233a4cec115520d2b0a22d07cdfe6a
stefan.mckinnon.edwards@gmail.com <stefan.mckinnon.edwards@gmail.com@90c1e3d9-ed51-6ea5-a3b1-c9ac2e7020b9>
2012-09-04 20:45:54 +0000

    Initial development. Indexing of links-flatfile does not work yet.

---
+++ initial_devel/settings.py
@@ -0,0 +1,8 @@
+
+STRING_version = '9.0'
+links_url = 'http://string-db.org/newstring_download/protein.links.v9.0.txt.gz'
+links_fn_gz = 'protein.links.v9.0.txt.gz'
+links_fn_bz2 = 'protein.links.v9.0.txt.bz2'
+lines_index_fn = 'protein.links.v9.0.idx'
+detailed = 'http://string-db.org/newstring_download/protein.links.detailed.v9.0.txt.gz'
+
