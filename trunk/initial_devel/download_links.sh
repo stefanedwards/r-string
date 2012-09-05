e7602e97be60c502c4eb9d42a7e9868969f23348
stefan.mckinnon.edwards@gmail.com <stefan.mckinnon.edwards@gmail.com@90c1e3d9-ed51-6ea5-a3b1-c9ac2e7020b9>
2012-09-05 11:24:18 +0000

    Fixed download and repacking to bz2. Modified to be used with qsub system.

--- trunk/initial_devel/download_links.sh
+++ trunk/initial_devel/download_links.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+#PBS -l walltime=03:00:00
+#PBS -j oe
 
-GZ_FILE = 'protein.links.v9.0.txt'
+opts=''
+## Sometimes the script is run with the qsub-system, so we add some flavours.
+if [ -n $PBS_O_WORKDIR ]; then
+cd $PBS_O_WORKDIR
+opts='-nv'
+fi
+GZ_FILE='protein.links.v9.0.txt'
 
-wget http://string-db.org/newstring_download/$GZ_FILE.gz
+echo Fetching $GZ_FILE
+
+if [ ! -f $GZ_FILE.gz ]; then
+  wget $opts http://string-db.org/newstring_download/$GZ_FILE.gz
+fi
+
 gunzip -c -d $GZ_FILE.gz | bzip2 -z > $GZ_FILE.bz2
 
