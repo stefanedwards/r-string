5175970eb79b73929cbaa26a8cc90af1f1d6f052
stefan.mckinnon.edwards@gmail.com <stefan.mckinnon.edwards@gmail.com@90c1e3d9-ed51-6ea5-a3b1-c9ac2e7020b9>
2012-09-05 20:08:58 +0000

    Index links works as desired.

--- trunk/initial_devel/settings.py
+++ trunk/initial_devel/settings.py
@@ -1,8 +1,14 @@
+import time
+
+print('Script started', time.strftime('%a %b %d %H:%M:%S %Y'))
 
 STRING_version = '9.0'
 links_url = 'http://string-db.org/newstring_download/protein.links.v9.0.txt.gz'
 links_fn_gz = 'protein.links.v9.0.txt.gz'
 links_fn_bz2 = 'protein.links.v9.0.txt.bz2'
-lines_index_fn = 'protein.links.v9.0.idx'
+links_idx_fn = 'protein.links.v9.0.idx'
+links_ind_fn = 'protein.links.v9.0.index'
 detailed = 'http://string-db.org/newstring_download/protein.links.detailed.v9.0.txt.gz'
-
+taxdmp_url = ''
+taxdmp_fn = 'taxdmp.zip'
+links_tbl_tpl = 'protein.links.v9.0.{}'
