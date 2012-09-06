25243577b93219ed11d206b70e2eeecc3168d8f0
stefan.mckinnon.edwards@gmail.com <stefan.mckinnon.edwards@gmail.com@90c1e3d9-ed51-6ea5-a3b1-c9ac2e7020b9>
2012-09-06 12:31:20 +0000

    Working draft for creating sqlite database.

--- initial_devel/settings.py
+++ initial_devel/settings.py
@@ -1,6 +1,10 @@
 import time
+from Path import Path
 
-print('Script started', time.strftime('%a %b %d %H:%M:%S %Y'))
+def print_time(prepend, **kwargs):
+  print(prepend, time.strftime('%a %b %d %H:%M:%S %Y'), **kwargs)
+ 
+print_time('Script started')
 
 STRING_version = '9.0'
 links_url = 'http://string-db.org/newstring_download/protein.links.v9.0.txt.gz'
@@ -12,3 +16,11 @@ detailed = 'http://string-db.org/newstring_download/protein.links.detailed.v9.0.
 taxdmp_url = ''
 taxdmp_fn = 'taxdmp.zip'
 links_tbl_tpl = 'protein.links.v9.0.{}'
+sqlite_db_tpl = 'STRING.v.9.0.{}.sqlite3'
+
+R_package_path = Path('/home/stefan/R/i686-pc-linux-gnu-library/2.14')
+taxonomies = dict()
+taxonomies[9606] = {'short':'Hs', 'bioc':'org.Hs.eg.db', 'db':'extdata/org.Hs.eg.sqlite', 'primary encoding':'ensembl'}  # Human
+taxonomies[9913] = {'short':'Bt', 'bioc':'org.Bt.eg.db', 'db':'extdata/org.Bt.eg.sqlite', 'primary encoding':'ensembl'}  # Bos taurus
+taxonomies[10090] = {'short':'Mm', 'bioc':'org.Mm.eg.db', 'db':'extdata/org.Mm.eg.sqlite', 'primary encoding':'ensembl'}  # Mouse
+#BioC db packages version: R_package_path / taxonomies[taxid]['bioc'] / DESCRIPTION $ Version
