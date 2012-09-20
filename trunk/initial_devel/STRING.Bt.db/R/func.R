8e0667adae05c480aa8ad58d9663f98ff83bb7b6
stefan.mckinnon.edwards@gmail.com <stefan.mckinnon.edwards@gmail.com@90c1e3d9-ed51-6ea5-a3b1-c9ac2e7020b9>
2012-09-20 11:16:31 +0000

    Updated STRING.Bt.db to work alone, and with working sqlite-connection.

--- trunk/initial_devel/STRING.Bt.db/R/func.R
+++ trunk/initial_devel/STRING.Bt.db/R/func.R
@@ -18,17 +18,63 @@
 
 NULL
 
-require(RSQLite)
-require(STRING.db)
 
-.db_name <- system.file('protein.links.v9.0.9913.sqlite3', package='STRING.Bt.db')
-.conn = dbConnect(dbDriver('SQLite'), .db_name)
+#require(RSQLite)
+#require(STRING.db)
 
+datacache <- new.env(hash=TRUE, parent=emptyenv())
 
-STRING.Bt.Meta <- function(key = '%') {
-  STRING.db:::.getMeta(.conn, key=key)
+# methods from AnnDbObj-lowAPI.R in AnnotationDbi
+#setMethod("dbconn", "environment", function(x) get("dbconn", envir=x))
+#setMethod("dbfile", "environment", function(x) get("dbfile", envir=x))
+dbconn <- function(x) get('dbconn', envir=x)
+dbfile <- function(x) get('dbfile', envir=x)
+
+# and we continue on our own
+STRING.Bt_dbconn <- function() dbconn(datacache)
+STRING.Bt_dbfile <- function() dbfile(datacache)
+
+STRING.Bt_organism <- 'Bos taurus'
+
+.onLoad <- function(libname, pkgname) {
+  require(RSQLite)
+  dbfile <- system.file('extdata','protein.links.v9.0.9913.sqlite3', package=pkgname, lib.loc=libname, mustWork=TRUE)
+  assign("dbfile", dbfile, envir=datacache)
+  
+  dbconn <- dbConnect(dbDriver('SQLite'), dbfile)
+  assign("dbconn", dbconn, envir=datacache)
 }
 
+.onUnload <- function(libpath) {
+  dbDisconnect(STRING.Bt_dbconn())
+}
+
+#' Retrieve metadata on the compiled data.
+#' 
+#' Query for all or specific values of the metadata.
+#' @param key Character vector of keys; supports sqlite's wildcards (% for any number of any characters, _ for single character wildcard). Defaults to retrieve all meta data.
+#' @return List with entries named by the key; return value is reduced to character vector if only one key is retrieved.
+#' @author Stefan McKinnon Edwards \email{stefan.hoj-edwards@@agrsci.dk}
+#' @export
+#' @examples
+#' STRING.Bt.Meta('Primary encoding')
+#' STRING.Bt.Meta('entrez%')
+STRING.Bt.Meta <- function(key = '%') {
+  .getMeta(dbconn(datacache), key=key)
+}
+
+#' Retrieve protein-protein interactions.
+#' 
+#' @param proteins Character vector of gene/protein names to query for.
+#' @param cutoff Integer, only retrieves interaction that scores larger than or equal to the cutoff value. Range 1-999.
+#' @param as.list Logical, restructure result to a list, see Value.
+#' @param simplify Logical, should function only return a character vector of all interaction partners? Overrules \code{as.list}.
+#' @return For \code{as.list} as \code{FALSE}, data.frame of three columns (\code{g1}, \code{g2}, \code{score}).
+#'         For \code{as.list} as \code{TRUE}, list with entries named by \code{g1} containing all mappings in \code{g2}. \code{score} is lost.
+#'         If \code{simplify} is \code{TRUE}, \code{as.list} is overruled and the returned value is a character vector.
+#' @author Stefan McKinnon Edwards \email{stefan.hoj-edwards@@agrsci.dk}
+#' @export
+#' @example STRING.Bt.PPI('ENSBTAP00000000017')
 STRING.Bt.PPI <- function(proteins, cutoff=900, as.list=FALSE, simplify=FALSE) {
-  STRING.db:::.getPPI(.conn, proteins=proteins, cutoff=cutoff, encoding="ensembl", as.list=as.list, simplify=simplify)
+  STRING.db:::.getPPI(dbconn(datacache), proteins=proteins, cutoff=cutoff, encoding="ensembl", as.list=as.list, simplify=simplify)
 }
