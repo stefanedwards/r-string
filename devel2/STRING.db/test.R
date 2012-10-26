36f120b70cdc2dbbfc38fc7a83abf24b843b35ba
stefan.mckinnon.edwards@gmail.com <stefan.mckinnon.edwards@gmail.com@90c1e3d9-ed51-6ea5-a3b1-c9ac2e7020b9>
2012-10-26 06:54:38 +0000

    Updated test routine in devel2, now working except mappings to entrez...

--- devel2/STRING.db/test.R
+++ devel2/STRING.db/test.R
@@ -1,12 +1,23 @@
+#!/opt/ghpc/bin/Rscript --vanilla 
+#PBS -j oe
+#PBS -l walltime=04:00:00
 ### Testing the application of STRING.db
+
+setwd(Sys.getenv('PBS_O_WORKDIR', '.'))
 
 install.packages(normalizePath('STRING.db'), repos=NULL, type='source')
 library(STRING.db)
 
 taxonomies <- c(9606,9913)
 settings <- www.settings(STRING.db::Latest.STRING.version)
-res <- index.flatfile(settings, taxonomies=taxonomies)
-#for (taxo in taxonomies) {
-#  makePackage(taxo, res$fn[taxo], STRING.db::Latest.STRING.version)
-#}
+print('Testing index.flatfile.')
+#res <- index.flatfile(settings, taxonomies=taxonomies)
+
+print('Testing extract.flatfile.')
+res <- extract.flatfile(settings, taxonomies=taxonomies)
+
+print(res)
+for (taxo in taxonomies) {
+  makePackage(taxo, res$fn[taxo], STRING.db::Latest.STRING.version)
+}
 
